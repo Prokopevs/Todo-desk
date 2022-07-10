d25f6fad3749b723d0533d6adffcca72afa37838
GeorgeandSavva <prokopev_savva@mail.ru>
2022-07-10 19:14:45 +0300

    login

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NeuToggle } from "neumorphism-react";
+import { NeuToggle } from 'neumorphism-react';
 
 const LoginForm = () => {
     return (
@@ -9,9 +9,13 @@ const LoginForm = () => {
                 <div className="block__line block__line-form"></div>
 
                 <p className="login__form_data_name">Email</p>
-                <input placeholder="Your email..." className="login__form_input"></input>
+                <div className="login__firm_input_holder">
+                    <input placeholder="Your email..." type="email" className="login__form_input"></input>
+                </div>
                 <p className="login__form_data_name">Password</p>
-                <input placeholder="Your account password" className="login__form_input"></input>
+                <div className="login__firm_input_holder">
+                    <input placeholder="Your account password..." type="password" className="login__form_input"></input>
+                </div>
 
                 <div className="login__form_remember">
                     <NeuToggle size="small" onChange={(value) => console.log("new toggle value : ", value)} />
