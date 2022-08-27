12fc56cecc4e3a22c05d9708dd9b708098a10948
GeorgeandSavva <prokopev_savva@mail.ru>
2022-08-27 22:42:17 +0300

    add isDragDisabled

--- src/components/Desk/Tasks.tsx
+++ src/components/Desk/Tasks.tsx
@@ -3,54 +3,49 @@ import { Draggable } from "react-beautiful-dnd"
 import { ITasksProps } from "../../models/Task/ITasksProps"
 import { useAppSelector } from "../../hooks/redux"
 import Task from "./Task"
-import { selectEditMode } from "../../Store/selectors";
+import { selectEditMode } from "../../Store/selectors"
 
-const Tasks: React.FC<ITasksProps> = React.memo(
-    ({ task, index, priorityArray, column }) => {
-        const { editArray } = useAppSelector(selectEditMode)
-        const [hover, setHover] = React.useState("")
+const Tasks: React.FC<ITasksProps> = ({ task, index, priorityArray, column }) => {
+    const { editArray } = useAppSelector(selectEditMode)
+    const [hover, setHover] = React.useState("")
 
-        const handleMouseOver = (id: string) => {
-            setHover(id)
-        }
-
-        const handleMouseOut = (id: string) => {
-            setHover("")
-        }
-
-        return (
-            <>
-                {!editArray?.includes(task.id) ? (
-                    <Draggable draggableId={task.id} index={index}>
-                        {(provided) => (
-                            <div
-                                className="block__inner_todo"
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                ref={provided.innerRef}
-                                onMouseOver={() => {handleMouseOver(task.id)}}
-                                onMouseOut={() => {handleMouseOut(task.id)}}
-                            >
-                                <Task task={task} priorityArray={priorityArray} column={column} hover={hover}/>
-                            </div>
-                        )}
-                    </Draggable>
-                ) : (
-                    <Draggable draggableId={task.id} index={index}>
-                        {(provided) => (
-                            <div
-                                className="block__inner_todo"
-                                {...provided.draggableProps}
-                                ref={provided.innerRef}
-                            >
-                                <Task task={task} priorityArray={priorityArray} column={column}/>
-                            </div>
-                        )}
-                    </Draggable>
-                )}
-            </>
-        )
+    const handleMouseOver = (id: string) => {
+        setHover(id)
     }
-)
+
+    const handleMouseOut = (id: string) => {
+        setHover("")
+    }
+
+    return (
+        <Draggable
+            draggableId={task.id}
+            index={index}
+            isDragDisabled={editArray?.includes(task.id)}
+        >
+            {(provided) => (
+                <div
+                    className="block__inner_todo"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    onMouseOver={() => {
+                        handleMouseOver(task.id)
+                    }}
+                    onMouseOut={() => {
+                        handleMouseOut(task.id)
+                    }}
+                >
+                    <Task
+                        task={task}
+                        priorityArray={priorityArray}
+                        column={column}
+                        hover={hover}
+                    />
+                </div>
+            )}
+        </Draggable>
+    )
+}
 
 export default Tasks
