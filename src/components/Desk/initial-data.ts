3f4f084232b287cc08eb0f9a0a9fb8af3bdebd58
Prokopevs <prokopev_savva@mail.ru>
2022-09-23 00:13:48 +0300

    changed initial-data file

--- src/components/Desk/initial-data.ts
+++ src/components/Desk/initial-data.ts
@@ -1,9 +1,11 @@
 const initialData = {
     tasks: {
-      "0": { id: "0", content: "Takeoutthegarbageandtosomeshitshitshitshitshitffsdafsdafsadfsadfsadfsad fdsafdsafsda fsdafdsafsdaf fasdfsda", priority: 1, isOpen: false },
-      "1": { id: "1", content: "Watch my favorite", priority: 1, isOpen: false },
-      "2": { id: "2", content: "Charge my phone", priority: 2, isOpen: false },
-      "3": { id: "3", content: "fuck you up ohohofuck you up ohoho", priority: 4, isOpen: false },
+      "0": { id: "0", content: "You can Drag-and-drop it", priority: 1, isOpen: false },
+      "1": { id: "1", content: "Try to drag Try to drag Try to drag Try to drag", priority: 3, isOpen: false },
+      "2": { id: "2", content: "You can also click on the status name to add a status or long press on it to change its name", priority: 2, isOpen: false },
+      "3": { id: "3", content: "Hover over and try to change the priority and task text", priority: 4, isOpen: false },
+      "4": { id: "4", content: "Simplicity is the ultimate sophistication (Leonardo da Vinci)", priority: 5, isOpen: false },
+      "5": { id: "5", content: "Сlick on the plus to add a task", priority: 2, isOpen: false },
     },
     columns: {
       "1": {
@@ -14,7 +16,7 @@ const initialData = {
       "2": {
         id: "2",
         name: "In progress",
-        taskIds: [],
+        taskIds: ["4", "5"],
       },
       "3": {
         id: "3",
